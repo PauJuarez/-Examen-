b3d291400333e0e36b77d706596cabe13d1a9d5d
PauJuarez <pjuarezc@inscastellet.cat>
2024-03-15 14:47:19 +0100

    mini coreccion < 0 a <=0

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -27,7 +27,7 @@ public class BalancedBrackets {
             if (ch == '[') {
                 brackets++;
         } else if (ch == ']') {
-            if (brackets < 0) {
+            if (brackets <= 0) {
                 return false;
             }
         }
